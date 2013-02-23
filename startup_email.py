4acc0c79a5a891680c0e95ecd4db74c29b15e5fa
Peter Dawson <petedaws@gmail.com>
2013-02-23 12:18:32 +1000

    added emailer start script

---
+++ startup_email.py
@@ -0,0 +1,31 @@
+#! /usr/bin/python
+
+import smtplib
+import time
+import socket
+import sys
+from email.mime.text import MIMEText
+
+try:
+	conf = eval(open(sys.argv[1],'rb').read())
+except:
+	print 'config file error'
+	exit()
+
+name = socket.gethostname()
+time = time.strftime('%Y-%m-%d %H:%M',time.localtime())
+output = '%s has started at %s.' % (name,time)
+msg = MIMEText(output)
+
+msg['Subject'] = '%s Remote: Started' % (name)
+msg['From'] = conf['email_source']
+
+s = smtplib.SMTP(conf['email_server'])
+s.starttls()
+s.login(conf['email'],conf['password'])
+s.sendmail(conf['email_source'],[conf['email_out']],msg.as_string())
+s.quit()
+
+#Add to rc.d
+# sudo ln -fs /home/pi/scripts/startup_email.py /etc/init.d/startup_email
+# sudo update-rc.d startup_email defaults
