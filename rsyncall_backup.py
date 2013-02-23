f6eda62c0351cf49444be66eb7786a2fb7234528
Peter Dawson <petedaws@gmail.com>
2013-02-23 11:37:01 +1000

    initial commit

---
+++ rsyncall_backup.py
@@ -0,0 +1,41 @@
+#! /usr/bin/python
+
+import subprocess
+import smtplib
+from email.mime.text import MIMEText
+
+try:
+	conf = eval(sys.argv[1])
+except:
+	exit()
+	
+if not conf['password']:
+	conf['password'] = open('%s.password' % conf['email'],'rb').read()
+
+for backup in conf['backups']:
+	command = 'rsync -avz -e "ssh -p %i" %s@%s:%s %s' % (conf['port'],
+	                                                     conf['user'],
+														 conf['host'],
+														 backup['src'],
+														 backup['dst'])
+	output += 'Running: ' + command + '\n'
+	proc = subprocess.Popen(command,shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+	stdout,stderr = proc.communicate()
+	output += '%s\n%s\n' % (stdout,stderr)
+	if len(stderr) > 0:
+		output += 'Fail' + '\n\n'
+		subject += 'Fail:'
+	else:
+		output += 'Complete' + '\n\n'
+		subject += 'Pass:'
+
+msg = MIMEText(output)
+
+msg['Subject'] = 'Backup: %s' % (subject)
+msg['From'] = conf['email_source']
+
+s = smtplib.SMTP(conf['email_server'])
+s.starttls()
+s.login(conf['email'],password)
+s.sendmail(conf['email_source'],[conf['email_out']],msg.as_string())
+s.quit()
